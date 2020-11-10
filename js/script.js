2cac7b1b5bb9eb0b6a2573d7be892cac1db03dca
Caleb Sonnichsen <csonnic03@icloud.com>
2020-11-09 18:40:44 -0700

    Added code for slideshow in script.js and styles.css

--- js/script.js
+++ js/script.js
@@ -13,3 +13,19 @@ document.querySelectorAll("footer")[0].style.color = "white";
 
 // Set the Footer Element to be center
 document.querySelectorAll("footer")[0].style.textAlign = "center";
+
+// Slideshow
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
